6580bb60a936e68e967ed1884140d31b0156d3a1
TanDat-2005 <tandatnguyen.25122000@gmail.com>
2026-05-13 16:01:42 +0700

    Them bao cao

---
+++ test/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = "TanDat";
+  return (
+
+
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Hello {name}
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
